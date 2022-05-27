874c6f0b6183cf84ea393ce7d73f6d6cf9dda0e3
sebadev-img <sebastiandev.img@gmail.com>
2022-05-27 00:37:30 -0300

    changed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fileUpload = require("express-fileupload");
 const pdfParse = require("pdf-parse");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use("/", express.static("public"));
 app.use(fileUpload());
